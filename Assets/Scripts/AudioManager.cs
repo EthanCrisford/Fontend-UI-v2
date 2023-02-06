7971507916a48944e4a15c6a7f87163c4bddb0c6
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-02-06 10:41:05 +0000

    1

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] Slider volumeSlider;
+    [System.Serializable]
+    public class Sound
+
+    {
+        public string name;
+
+        public AudioClip clip;
+
+        [Range(0f, 1f)]
+        public float volume;
+        [Range(1f, 3f)]
+        public float pitch;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+
     void Start()
     {
-        
+        {
+            if (!PlayerPrefs.HasKey("musicVol"))
+            {
+                PlayerPrefs.SetFloat("musicVol", 1);
+                Load();
+            }
+            else
+            {
+                Load();
+            }
+        }
+    }
+    void Awake()
+    {
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if (musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+            AudioListener.volume = volumeSlider.value;
+
+        }
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeVol()
     {
-        
+        AudioListener.volume = volumeSlider.value;
+        Save();
+
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVol", volumeSlider.value);
+    }
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVol");
     }
 }
+
